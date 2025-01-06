df3e55c2272a06c49201c76175af3b6742733405
Vincent Brunet <vincent.brunet.us@gmail.com>
2025-01-06 15:29:10 +0100

    nit-import

--- solana_toolbox_anchor/src/lib.rs
+++ solana_toolbox_anchor/src/lib.rs
@@ -4,9 +4,8 @@ mod toolbox_anchor_error;
 mod toolbox_anchor_idl;
 mod toolbox_anchor_instruction;
 
+pub use solana_toolbox_endpoint::ToolboxEndpoint;
 pub use toolbox_anchor_account::get_account_data_anchor_deserialized;
 pub use toolbox_anchor_error::ToolboxAnchorError;
 pub use toolbox_anchor_instruction::process_anchor_instruction;
 pub use toolbox_anchor_instruction::process_anchor_instruction_with_signers;
-
-pub use solana_toolbox_endpoint::ToolboxEndpoint;
