b9181cf208a73f598735e80b40982dd6d2115e91
Vincent Brunet <vincent.brunet.us@gmail.com>
2025-01-09 19:11:13 +0100

    0.1.27-solana-1.18.26-anchor-0.30.1

---
+++ solana_toolbox_anchor/tests/suites/devnet_sysvars.rs
@@ -0,0 +1,26 @@
+use solana_sdk::commitment_config::CommitmentConfig;
+use solana_toolbox_anchor::ToolboxAnchorEndpoint;
+use solana_toolbox_anchor::ToolboxEndpoint;
+use solana_toolbox_anchor::ToolboxEndpointLoggerPrint;
+
+#[tokio::test]
+pub async fn devnet_sysvars() {
+    // Create the devnet endpoint
+    let mut endpoint = ToolboxAnchorEndpoint::from(
+        ToolboxEndpoint::new_rpc_with_url_and_commitment(
+            "https://api.devnet.solana.com".to_string(),
+            CommitmentConfig::confirmed(),
+        ),
+    );
+    // Create a print logger
+    endpoint.add_logger(Box::new(ToolboxEndpointLoggerPrint::new()));
+    // Fetch the sysvars
+    let clock = endpoint.get_sysvar_clock().await;
+    let rent = endpoint.get_sysvar_rent().await;
+    // Check that the accounts are fetched properly
+    assert!(clock.is_ok());
+    assert!(rent.is_ok());
+    let rent = rent.unwrap();
+    assert_eq!(3480, rent.lamports_per_byte_year);
+    assert_eq!(2.0, rent.exemption_threshold);
+}
--- solana_toolbox_anchor/tests/suites/mod.rs
+++ solana_toolbox_anchor/tests/suites/mod.rs
@@ -1,3 +1,4 @@
+pub mod devnet_sysvars;
 pub mod program_test_airdrop_transfer_get;
 pub mod program_test_builtin_program;
 pub mod program_test_logger_print;
